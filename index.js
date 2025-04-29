fc4e0cf9b485c8dcfbd05372e44605ae24ddea69
ViLP357 <vilipi2009@gmail.com>
2025-04-29 16:07:31 +0300

    cors lisätty

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require("dotenv").config()
 const express = require('express')
 const Score = require('./models/score')
 const app = express()
+const cors = require("cors")
+
+app.use(cors())
 //const mongoose = require("mongoose")
 
 app.use(express.json())
